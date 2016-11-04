0ea334c180ca16a8dc52a5475380794fb1d69661
Johnny Chau <jtc003@ucsd.edu>
2016-11-03 22:18:09 -0700

    Notifications/Add Tasks data

---
+++ public/js/remind.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+
+	
+	$("form #submitBtn").click(function(e){
+		e.preventDefault;
+		alert("Reminder sent!");
+	});
+})
--- routes/group2.js
+++ routes/group2.js
@@ -1,3 +1,4 @@
+var data = require('../data.json');
 exports.view = function(req, res){
-	res.render('group');
+	res.render('group', data);
 }
