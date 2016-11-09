0bc1be49685c48c45ad2d58fe6fcaaaf81346cce
Crystal Jiao <crystaljiao96@gmail.com>
2016-11-08 21:54:19 -0800

    fixed having a back button on the nav bar in mobile mode, and not in the hamburger menu for all pages

--- routes/addTask.js
+++ routes/addTask.js
@@ -3,3 +3,6 @@ var data = require('../data.json');
 exports.view = function(req, res){
 	res.render('addTask');
 }
+// we can make the task turn red and be marked as "Completed" and then move it 
+// to the bottom of the list of tasks
+
